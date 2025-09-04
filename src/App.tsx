c69592fe1b25908acc1f25c219d93d7d5b77e56b
Dikla002 <reljadikla@gmail.com>
2025-09-04 18:37:32 +0200

    Remove lovable-tagger dependency causing build failure

--- src/App.tsx
+++ src/App.tsx
@@ -23,6 +23,7 @@ const App = () => (
       <LanguageProvider>
         <SearchProvider>
           <FavoritesProvider>
+            
             <TooltipProvider>
               <Toaster />
               <Sonner />
